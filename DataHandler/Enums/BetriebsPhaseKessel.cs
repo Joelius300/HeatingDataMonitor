fb2d255a77bc45ec19aa05353035189cb372ab6a
jli103828 <39618498+jli103828@users.noreply.github.com>
2019-05-13 22:28:03 +0200

    Add sure flags for enum ints

--- DataHandler/Enums/BetriebsPhaseKessel.cs
+++ DataHandler/Enums/BetriebsPhaseKessel.cs
@@ -8,13 +8,13 @@ namespace DataHandler.Enums
     // int values are not according to order in specs (p. 44)
     public enum BetriebsPhaseKessel
     {
-        Aus = 0,
-        Anheizen = 1,
-        Automatik = 2,
+        Aus = 0,                // sure
+        Anheizen = 1,           // sure
+        Automatik = 2,          // sure
         Anheizen_erkennen = 3,
-        Abregeln = 4,
+        Ausbrennen = 4,         // sure
         Gluterhaltung = 5,
-        Ausbrennen = 6,
+        Abregeln = 6,           
         Uebertemperatur = 7,
         Tuer_Geoeffnet = 8
     }
