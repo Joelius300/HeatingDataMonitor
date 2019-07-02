af5cc66afb00311fc6f28189c6d041786035bd7e
jli103828 <39618498+jli103828@users.noreply.github.com>
2019-07-02 22:14:03 +0200

    Update to preview6, add HistoryRepository (not tested yet)

---
+++ DataHistory/HistoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataHandler;
+
+namespace DataHistory
+{
+    public class HistoryRepository : IHistoryRepository
+    {
+        public IEnumerable<Data> GetDataBetween(DateTime from, DateTime to)
+        {
+            if (HeatingDataContext.Instance == null) throw new InvalidOperationException("The Database-context has not yet been initialized.");
+            return HeatingDataContext.Instance.Data.Where(d => (d.DatumZeit.HasValue && d.DatumZeit.Value >= from && d.DatumZeit.Value <= to));
+        }
+    }
+}
---
+++ DataHistory/IHistoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using DataHandler;
+using System.Collections.Generic;
+
+namespace DataHistory
+{
+    interface IHistoryRepository
+    {
+        IEnumerable<Data> GetDataBetween(DateTime from, DateTime to);
+    }
+}
