5bb4cb77c2377c25b884400e907f8057b6e41126
Joelius300 <bstlng321@gmail.com>
2020-04-29 17:26:03 +0200

    Add extension to register SerialPortHeatingDataReceiver

---
+++ backend/HeatingDataMonitor.Data/Service/HeatingDataReceiverServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using HeatingDataMonitor.Service;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    // Inspired by the ..ServiceCollectionExtensions in the aspnetcore middlewares
+    public static class HeatingDataReceiverServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSerialPortHeatingDataReceiver(this IServiceCollection services)
+        {
+            services.AddOptions(); // add options if not done yet
+
+            // If IConfigureOptions<SerialPortOptions> was already registered, this doesn't do anything.
+            services.TryAddTransient<IConfigureOptions<SerialPortOptions>, SerialPortOptionsSetup>();
+
+            services.AddSingleton<IHeatingDataReceiver, SerialPortHeatingDataReceiver>();
+            services.AddHostedService(sp => sp.GetRequiredService<IHeatingDataReceiver>());
+
+            return services;
+        }
+    }
+}
