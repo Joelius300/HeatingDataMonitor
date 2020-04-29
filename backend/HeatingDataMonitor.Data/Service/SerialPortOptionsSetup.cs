8e733b9440bfe04d30c0149062973eea9a7cd2c2
Joelius300 <bstlng321@gmail.com>
2020-04-29 17:25:37 +0200

    Add options for serial port

---
+++ backend/HeatingDataMonitor.Data/Service/SerialPortOptionsSetup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+
+namespace HeatingDataMonitor.Service
+{
+    internal class SerialPortOptionsSetup : IConfigureOptions<SerialPortOptions>
+    {
+        public void Configure(SerialPortOptions options)
+        {
+            string[] availablePorts = SerialPort.GetPortNames();
+            if (availablePorts.Length == 0)
+                throw new InvalidOperationException("No available ports found on this machine.");
+
+            Array.Sort(availablePorts);
+            options.PortName = availablePorts[0];
+        }
+    }
+}
