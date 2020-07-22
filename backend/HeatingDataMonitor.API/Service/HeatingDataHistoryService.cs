638d73aed822b54a1912865e11acb005a33e548c
Joelius300 <bstlng321@gmail.com>
2020-07-22 16:23:33 +0200

    Start the archiving cycle on the first received value

--- backend/HeatingDataMonitor.API/Service/HeatingDataHistoryService.cs
+++ backend/HeatingDataMonitor.API/Service/HeatingDataHistoryService.cs
@@ -16,7 +16,7 @@ namespace HeatingDataMonitor.API.Service
 {
     public class HeatingDataHistoryService : BackgroundService
     {
-        private const int ArchiveDelay = 60 * 1000; // TODO: FROM CONFIG?
+        private const int ArchiveDelay = 60 * 1000;
         private readonly ILogger<HeatingDataHistoryService> _logger;
         private readonly IHubContext<HeatingDataHub, IHeatingDataHubClient> _hubContext;
         private readonly IHeatingDataReceiver _heatingDataReceiver;
@@ -36,18 +36,25 @@ namespace HeatingDataMonitor.API.Service
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+            _heatingDataReceiver.DataReceived += ReceivedHandler;
+
+            // Wait for the first time DataReceived is fired, then start
+            // the fixed time loop.
+            await tcs.Task;
+            await ExecuteLoop(stoppingToken);
+
+            void ReceivedHandler(object o, HeatingData e)
+            {
+                tcs.SetResult(true);
+                _heatingDataReceiver.DataReceived -= ReceivedHandler;
+            }
+        }
+
+        private async Task ExecuteLoop(CancellationToken stoppingToken)
+        {
             while (!stoppingToken.IsCancellationRequested)
             {
-                try
-                {
-                    await Task.Delay(ArchiveDelay, stoppingToken);
-                }
-                catch (TaskCanceledException e)
-                {
-                    _logger.LogDebug(e.Message);
-                    break;
-                }
-
                 HeatingData current = _heatingDataReceiver.Current;
                 if (current != null && !ReferenceEquals(_lastAdded, current))
                 {
@@ -64,6 +71,16 @@ namespace HeatingDataMonitor.API.Service
 
                     await _hubContext.Clients.All.OnDataPointArchived(current);
                     _logger.LogDebug($"Archived data with timestamp: {current.ReceivedTime}");
+                }
+
+                try
+                {
+                    await Task.Delay(ArchiveDelay, stoppingToken);
+                }
+                catch (TaskCanceledException e)
+                {
+                    _logger.LogDebug(e.Message);
+                    break;
                 }
             }
         }
