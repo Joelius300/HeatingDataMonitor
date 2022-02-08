5eaaddf64fecdb89316c0c541cab37f615eddb13
Joel L <bstlng321@gmail.com>
2022-02-08 19:04:08 +0100

    test with actual serial port
    I am soo glad that this all works

--- backend/HeatingDataMonitor.Receiver/Program.cs
+++ backend/HeatingDataMonitor.Receiver/Program.cs
@@ -2,6 +2,92 @@ using HeatingDataMonitor.Database;
 using HeatingDataMonitor.Receiver;
 using NodaTime;
 
+
+
+const int count = 3;
+var reader = _csvHeatingDataReader;
+
+Console.WriteLine("--- ReadCsvLines, cancel with break");
+
+int c = 0;
+await foreach (string s in reader.ReadCsvLines())
+{
+    Console.WriteLine($"New string from enumerable: {s}");
+    if (++c % count == 0)
+    {
+        Console.WriteLine("Manually cancelling now using break!");
+        break;
+    }
+}
+
+Console.WriteLine("--- ReadCsvLines, cancel after move next was called (inside body)");
+
+CancellationTokenSource cts = new();
+await foreach (string s in reader.ReadCsvLines().WithCancellation(cts.Token))
+{
+    Console.WriteLine($"New string from enumerable: {s}");
+    if (++c % count == 0)
+    {
+        Console.WriteLine("Manually cancelling now using cts.Cancel()!");
+        cts.Cancel();
+    }
+}
+cts.Dispose();
+
+Console.WriteLine("--- ReadCsvLines, cancel with token after 150ms");
+
+cts = new();
+cts.CancelAfter(TimeSpan.FromMilliseconds(150));
+await foreach (string s in reader.ReadCsvLines().WithCancellation(cts.Token))
+{
+    Console.WriteLine($"New string from enumerable: {s}");
+}
+cts.Dispose();
+
+Console.WriteLine("--- ReadCsvLines, cancel with token after 150ms AND break after second iteration (which should be after it was cancelled)");
+
+int o = 0;
+cts = new();
+cts.CancelAfter(TimeSpan.FromMilliseconds(150));
+await foreach (string s in reader.ReadCsvLines().WithCancellation(cts.Token))
+{
+    Console.WriteLine($"New string from enumerable: {s}");
+    if (++o >= 2)
+    {
+        // it should never reach this because the second call to MoveNextAsync is cancelled, and thus should return false, meaning only one iteration is performed
+        Console.WriteLine("Breaking after second iteration (actually second call to MoveNextAsync)");
+        break;
+    }
+}
+cts.Dispose();
+
+Console.WriteLine("--- ReadCsvLines, store and enumerate twice (UNDEFINED PER GUIDELINES; WORKS IN OUR EXAMPLE)");
+
+IAsyncEnumerable<string> enumerable = reader.ReadCsvLines();
+
+Console.WriteLine("First iteration:");
+await foreach (string s in enumerable)
+{
+    Console.WriteLine($"New string from enumerable: {s}");
+    if (++c % count == 0)
+    {
+        Console.WriteLine("Manually cancelling now using break.");
+        break;
+    }
+}
+
+Console.WriteLine("Second iteration:");
+await foreach (string s in enumerable)
+{
+    Console.WriteLine($"New string from enumerable: {s}");
+    if (++c % count == 0)
+    {
+        Console.WriteLine("Manually cancelling now using break.");
+        break;
+    }
+}
+
+
 IHost host = Host.CreateDefaultBuilder(args)
     .UseSystemd() // TODO make sure to use Type=notify in Systemd file
     .ConfigureServices((context, services) =>
@@ -9,10 +95,11 @@ IHost host = Host.CreateDefaultBuilder(args)
         services.AddOptions<SerialPortOptions>()
                 .BindConfiguration("Serial");
 
-        services.AddHeatingDataDatabaseTimescaledb(context.Configuration.GetConnectionString("HeatingDataDatabase"));
         services.AddSingleton<IClock>(SystemClock.Instance);
-        services.AddSingleton<IHeatingDataReceiver, SerialPortHeatingDataReceiver>();
-        services.AddHostedService(sp => sp.GetRequiredService<IHeatingDataReceiver>());
+        services.AddSingleton<ICsvHeatingDataReader, SerialPortCsvHeatingDataReader>();
+        // services.AddHeatingDataDatabaseTimescaledb(context.Configuration.GetConnectionString("HeatingDataDatabase"));
+        //services.AddSingleton<IHeatingDataReceiver, SerialPortHeatingDataReceiver>();
+        //services.AddHostedService(sp => sp.GetRequiredService<IHeatingDataReceiver>());
 
         services.AddHostedService<Worker>();
     })
--- backend/HeatingDataMonitor.Receiver/Worker.cs
+++ backend/HeatingDataMonitor.Receiver/Worker.cs
@@ -3,18 +3,19 @@ namespace HeatingDataMonitor.Receiver;
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly ICsvHeatingDataReader _csvHeatingDataReader;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, ICsvHeatingDataReader csvHeatingDataReader)
     {
         _logger = logger;
+        _csvHeatingDataReader = csvHeatingDataReader;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        await foreach (string line in _csvHeatingDataReader.ReadCsvLines().WithCancellation(stoppingToken))
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+            _logger.LogInformation("Line received: {Line}", line);
         }
     }
 }
