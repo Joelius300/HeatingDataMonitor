22aeeb40d2c8b93b831c68fd867e1b5043e996b8
Joel L <bstlng321@gmail.com>
2022-03-04 17:41:03 +0100

    Almost Finish this thing
    Not tested, not 100% sure if it works.
    
    Things todo:
    - add logs (I want to see when it starts and stops streaming)

--- backend/HeatingDataMonitor.API/Service/HeatingDataRealTimeService.cs
+++ backend/HeatingDataMonitor.API/Service/HeatingDataRealTimeService.cs
@@ -1,81 +1,133 @@
-﻿using HeatingDataMonitor.API.Hubs;
+﻿using System.Diagnostics;
+using HeatingDataMonitor.API.Hubs;
 using Microsoft.AspNetCore.SignalR;
 using HeatingDataMonitor.Database.Models;
 using HeatingDataMonitor.Receiver.Shared;
 
 namespace HeatingDataMonitor.API.Service;
 
-/* Reiterating general idea because I thought this would be simple
- * but I'm struggling. You can probably take a lot of inspiration from BackgroundService.
- * When this service starts, it just starts listening for real-time connections.
+/* When this service starts, it starts listening for real-time connections.
  * When the first real-time connection started, start streaming to clients.
  * When the last real-time connection ended, stop streaming to clients.
- * When the service ends, stop listening, stop streaming and wait for task to finish unless forced shutdown happens.
+ * When the service stops, stop listening, stop streaming and wait for task to finish unless forced shutdown happens.
  */
-internal sealed class HeatingDataRealTimeService : IHostedService, IAsyncDisposable
+internal sealed class HeatingDataRealTimeService : BackgroundService
 {
     private readonly ILogger<HeatingDataRealTimeService> _logger;
     private readonly IHubContext<HeatingDataHub, IHeatingDataHubClient> _hubContext;
     private readonly IHeatingDataReceiver _heatingDataReceiver;
     private readonly IRealTimeConnectionManager _connectionManager;
-    private readonly IHostApplicationLifetime _applicationLifetime;
-    private Task? _streamingTask;
-    private CancellationTokenSource? _cts;
+    private readonly SemaphoreSlim _firstUserConnectedSemaphore;
+    private CancellationTokenSource? _lastUserDisconnectedCts;
 
     public HeatingDataRealTimeService(ILogger<HeatingDataRealTimeService> logger,
         IHubContext<HeatingDataHub, IHeatingDataHubClient> hubContext, IHeatingDataReceiver heatingDataReceiver,
-        IRealTimeConnectionManager connectionManager, IHostApplicationLifetime applicationLifetime)
+        IRealTimeConnectionManager connectionManager)
     {
         _logger = logger;
         _hubContext = hubContext;
         _heatingDataReceiver = heatingDataReceiver;
         _connectionManager = connectionManager;
-        _applicationLifetime = applicationLifetime;
+
+        // max 1 concurrent use and requires Release for first use
+        _firstUserConnectedSemaphore = new SemaphoreSlim(0, 1);
     }
 
-    private async Task StreamDataToClients(CancellationToken stoppingToken)
+    public override Task StartAsync(CancellationToken startupCancellationToken)
     {
-        await foreach (HeatingData heatingData in _heatingDataReceiver.StreamHeatingData(stoppingToken))
+        _connectionManager.FirstUserConnected += FirstClientConnected;
+        _connectionManager.LastUserDisconnected += LastClientDisconnected;
+
+        // starts ExecuteAsync in the background
+        return base.StartAsync(startupCancellationToken);
+    }
+
+    private void FirstClientConnected(object? sender, EventArgs e)
+    {
+        try
+        {
+            _firstUserConnectedSemaphore.Release(); // make WaitUntilFirstClientConnects return
+        }
+        catch (SemaphoreFullException)
+        {
+            // if this happens the error could be.
+            // - error in connection manager that allowed this event to fire twice without the other one in between
+            // - race condition: if this event was is called multiple times (maybe correctly so) while the ExecuteAsync
+            //   loop was wrapping around to start waiting for a new connection again, this could happen as well.
+            //   This case should be recoverable, as nothing is stopping the main loop from continuing (even though
+            //   it might go on for longer than necessary if a stop event was issued but the cancellation wasn't ready
+            //   yet so it was ignored. Check LastClientDisconnected as they are very closely related.
+            _logger.LogWarning("Tried to start streaming real-time data to clients but it was already started");
+        }
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken serviceStoppingToken)
+    {
+        while (!serviceStoppingToken.IsCancellationRequested)
+        {
+            await WaitUntilFirstClientConnects(serviceStoppingToken);
+
+            // create and store token which can be canceled from the last user disconnected event.
+            // we also link it with the serviceStoppingToken to ensure the application can gracefully shutdown
+            // even if there are still clients connected.
+            _lastUserDisconnectedCts = CancellationTokenSource.CreateLinkedTokenSource(serviceStoppingToken);
+            try
+            {
+                // stream data until token is canceled (which will return, not throw).
+                // all exceptions bubble up to the host which (by default) terminates the application gracefully.
+                await StreamDataToClients(_lastUserDisconnectedCts.Token);
+            }
+            finally
+            {
+                // ensure that the token stored in _lastUserDisconnectedCts is always valid for cancellation or null.
+                _lastUserDisconnectedCts.Dispose();
+                _lastUserDisconnectedCts = null;
+            }
+        }
+    }
+
+    private Task WaitUntilFirstClientConnects(CancellationToken cancellationToken) =>
+        _firstUserConnectedSemaphore.WaitAsync(cancellationToken);
+
+    private async Task StreamDataToClients(CancellationToken lastClientDisconnectedToken)
+    {
+        await foreach (HeatingData heatingData in _heatingDataReceiver.StreamHeatingData(lastClientDisconnectedToken))
         {
             await _hubContext.Clients.All.OnDataPointReceived(heatingData);
             _logger.LogTrace("Sent data to all clients with timestamp: {Timestamp}", heatingData.ReceivedTime);
         }
     }
 
-    public Task StartAsync(CancellationToken cancellationToken)
+    private void LastClientDisconnected(object? sender, EventArgs e)
     {
-        _connectionManager.FirstUserConnected += StartStreaming;
-        _connectionManager.LastUserDisconnected += StopStreaming;
-    }
-
-    private async void StartStreaming(object? sender, EventArgs e) => await StartStreaming();
-    private async Task StartStreaming()
-    {
-        if (_streamingTask is not null && _streamingTask.)
+        if (_lastUserDisconnectedCts is null)
         {
-            _logger.LogWarning("Real-time data streaming was started twice");
+            // if the cts is null when this event happens, the following things are possible:
+            // - error in connection manager allowed this event to be fired before start streaming event
+            // - race condition where this event fired almost instantly after the other one fired which made
+            //   WaitUntilFirstClientConnects return but before _lastUserDisconnectedCts was assigned.
+            //   This is recoverable but could cause the loop to go on for longer than necessary because this
+            //   event was swallowed instead of being buffered.
+            _logger.LogWarning("Tried to stop streaming real-time data to clients but it seemingly never started");
             return;
         }
 
-        _cts = new CancellationTokenSource();
-        _streamingTask = StreamDataToClients(_cts.Token);
+        _lastUserDisconnectedCts.Cancel();
     }
 
-    private void StopStreaming(object? sender, EventArgs e) => StopStreaming();
-    private void StopStreaming()
+    public override Task StopAsync(CancellationToken notGracefulAnymoreCancellationToken)
     {
-        if (_cts is null)
-        {
-            _logger.LogError("Real-time data streaming was stopped without being started");
-            return;
-        }
+        _connectionManager.FirstUserConnected -= FirstClientConnected;
+        _connectionManager.LastUserDisconnected -= LastClientDisconnected;
 
-        _cts.Cancel();
-        _cts.Dispose();
+        // this will cancel the serviceStoppingToken and wait for ExecuteAsync to end gracefully before killing it if necessary
+        return base.StopAsync(notGracefulAnymoreCancellationToken);
     }
 
-    public Task StopAsync(CancellationToken cancellationToken)
+    public override void Dispose()
     {
-
+        Debug.Assert(_lastUserDisconnectedCts is null, "_lastUserDisconnectedCts is null"); // already done
+        _firstUserConnectedSemaphore.Dispose();
+        base.Dispose();
     }
 }
