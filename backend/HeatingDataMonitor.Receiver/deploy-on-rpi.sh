35c95747cb294966fb15e0504144f50092fc9b7b
Joel L <joelius300@protonmail.com>
2022-08-29 13:45:43 +0200

    Update the PATH variable in the receiver install script
    In the docs it was stated that the install script would update the PATH itself but that only happens when it is sourced, not simply executed. Sourcing bashrc directly doesn't work either because there's a guard to prevent that for reasons.

--- backend/HeatingDataMonitor.Receiver/deploy-on-rpi.sh
+++ backend/HeatingDataMonitor.Receiver/deploy-on-rpi.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+set -e
+
 if [[ -z "$DOTNET_ROOT" ]]; then
   echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
   echo 'export DOTNET_CLI_TELEMETRY_OPTOUT=1' >> ~/.bashrc
   echo 'export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools' >> ~/.bashrc
+  # sourcing .bashrc from here doesn't work so update it for this process manually
   export DOTNET_ROOT=$HOME/.dotnet
   export DOTNET_CLI_TELEMETRY_OPTOUT=1
-  # PATH is updated for this process by the installer script
+  export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools  # if the .NET install script were sourced, this wouldn't be necessary
 fi
 
-# install (and maybe even updates?) .NET
+# installs (and maybe even updates?) .NET
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 6.0
 
 dotnet publish -c Release
